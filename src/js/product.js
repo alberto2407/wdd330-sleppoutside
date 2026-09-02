bed96d31b1660e7ea6bde7951c4a494e48097388
alberto2407 <albertofc2407@gmail.com>
2026-09-02 15:28:23 +0200

    Fix: Correct addProductToCart to save array to localStorage

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || []; // Get existing cart
+  cartItems.push(product); // Add new product
+  setLocalStorage("so-cart", cartItems); // Save the complete array
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
